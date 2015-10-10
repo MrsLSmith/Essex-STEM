4db28411837f4fb0c52815ec22d18e34376e54ef
dmeskill <dmeskill@ccsuvt.org>
2015-10-10 02:11:15 -0400

    hah!

--- Client/googlemaps.js
+++ Client/googlemaps.js
@@ -3,6 +3,9 @@
  */
 var map;
 
+var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var labelIndex = 0;
+
 window.mapDemo = window.mapDemo || {};
 
 // Adds a marker to the map.
@@ -15,7 +18,28 @@ function addMarker(location) {
         map: map,
 
     });
+/////////////////////
+    function initialize() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+        });
 
+    google.maps.event.addListener(map, 'click', function(event) {
+        addMarker(event.latLng, map);
+    });
+
+    // Adds a marker to the map.
+    function addMarker(location, map) {
+        // Add the marker at the clicked location, and add the next-available label
+        // from the array of alphabetical characters.
+        var marker = new google.maps.Marker({
+            position: location,
+            label: labels[labelIndex++ % labels.length],
+            map: map
+        });
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize)}
+////////////////////////
 }
 function centerMap(location){
     map.setCenter(location);
@@ -26,7 +50,7 @@ function initMap() {
      var myLatLng = {"lat":44.4763409,"lng":-73.2083652};
 
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 2,
+        zoom: 4,
         center: myLatLng
     });
 
