89ee5ba917b69474c1574c740c304f2efa067b0c
John Need <jneed@inntopia.com>
2015-10-10 04:08:50 -0400

    merge conflict

--- greenup.js
+++ greenup.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 MarkerList = new Mongo.Collection("markers");
 
 MarkerList.insert({lat: 2, long: 3, bags: 3, time: new Date()});
@@ -9,25 +8,11 @@ ESSEX.maps= {};
 ESSEX.maps.placePin = function(m){
     console.log(m.lat);
 };
-=======
-
-function getLocation() {
-    navigator.geolocation.getCurrentPosition(showPosition);
-}
-
-function showPosition(position) {
-    console.log(JSON.stringify({lat: position.coords.latitude, lng: position.coords.longitude}));
-    Session.set({lat: position.coords.latitude, lng: position.coords.longitude});
-    console.log(JSON.stringify(Session.get('lat')));
-    MarkerList.insert({lat: position.coords.latitude, lng: position.coords.longitude, bags: 3, time: new Date()});
-}
-
-MarkerList = new Mongo.Collection('markers');
->>>>>>> c196730efeca258b4cae3b0af2b2adff2df541fd
 
 if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
 
-<<<<<<< HEAD
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
@@ -43,24 +28,13 @@ if (Meteor.isClient) {
 
       }
   });
-=======
-    Template.hello.helpers({
-        counter: function () {
-            return Session.get('counter');
-        }
-    });
->>>>>>> c196730efeca258b4cae3b0af2b2adff2df541fd
 
   Template.hello.events({
-    'click .location-button': function () {
+    'click button': function () {
       // increment the counter when button is clicked
-
-        getLocation();
-
-        window.mapDemo.addMarker({lat: Session.get("lat"), lng: Session.get("lng")});
-        window.mapDemo.centerMap({lat: Session.get("lat"), lng: Session.get("lng")});
+      Session.set('counter', Session.get('counter') + 1);
     }
-  })
+  });
 }
 
 if (Meteor.isServer) {
