7673451a5a9043ea0b3889eb7d563d2753321d06
John Need <jneed@inntopia.com>
2015-10-10 03:51:21 -0400

    blah

--- greenup.js
+++ greenup.js
@@ -1,3 +1,14 @@
+MarkerList = new Mongo.Collection("markers");
+
+MarkerList.insert({lat: 2, long: 3, bags: 3, time: new Date()});
+
+
+ESSEX = {};
+ESSEX.maps= {};
+ESSEX.maps.placePin = function(m){
+    console.log(m.lat);
+};
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
@@ -5,7 +16,17 @@ if (Meteor.isClient) {
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
-    }
+    },
+      markers : function(){
+           var myMarkers =  MarkerList.find({});
+       var newArray =   myMarkers.map(function(marker){
+              return {lat: marker.lat, lng : marker.long};
+          });
+          newArray.forEach(function(marker){
+             ESSEX.maps.placePin(marker);
+          })
+
+      }
   });
 
   Template.hello.events({
