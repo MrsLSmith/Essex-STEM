adbfbbe8be65cdd00fa2dbf07de9a1acdac69022
parkkillerness <PFranz@ccsuvt.org>
2015-10-09 22:02:54 -0400

    Popups are in!

--- greenup.js
+++ greenup.js
@@ -1,23 +1,67 @@
+
+
+
+
+Markerlist = new Mongo.Collection('markers');
+Markerlist.insert(
+    {
+
+        lat: 2, long: 3, bags: 3, time: new Date()
+
+    });
+
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    // counter starts at 0
+    Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    Template.hello.helpers({
+        counter: function () {
+            return Session.get('counter');
+        },
+        markers: function () {
+            return Markerlist.find();
+        }
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+
+    });
+
+    Template.hello.events({
+        'click button': function () {
+            // increment the counter when button is clicked
+            window.prompt('How many bags');
+            var numberOfBags = (Session.set('numberOfBags', 1));
+        }
+    });
+
+
+    Template.gone.helpers({
+        counter: function () {
+            return Session.get('counter');
+        },
+        markers: function () {
+            return Markerlist.find();
+        }
+    });
+
+    Template.gone.events({
+        'click button': function () {
+            // increment the counter when button is clicked
+
+            var trashGone = window.confirm('Is the trash really gone?');
+
+
+        },
+    });
+
 }
+
+
+
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
+
