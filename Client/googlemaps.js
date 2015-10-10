1d8c4b1c3221d983c061d00d7cb312040f3a9573
dmeskill <dmeskill@ccsuvt.org>
2015-10-10 00:17:19 -0400

    maker without center

--- Client/googlemaps.js
+++ Client/googlemaps.js
@@ -1,3 +1,44 @@
 /**
- * Created by crazy_000 on 10/9/2015.
+ * Created by dan on 10/9/2015.
  */
+var map;
+
+window.mapDemo = window.mapDemo || {};
+
+// Adds a marker to the map.
+function addMarker(location) {
+    // Add the marker at the clicked location, and add the next-available label
+    // from the array of alphabetical characters.
+    var marker = new google.maps.Marker({
+        position: location,
+        label:"ME",
+        map: map
+    });
+}
+
+function initMap() {
+     var myLatLng = {lat: 1, lng: 131.044};
+
+    //var myMarkers = MapMarkerList.find();
+    //myMarkers = myMarkers.map(function(marker){
+    //    return {
+    //        lat: marker.lat,
+    //        lng: marker.lng
+    //    };
+    //})
+
+    ///var myLatLng = myMarkers[0];
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: myLatLng
+    });
+
+    //var marker = new google.maps.Marker({
+    //    position: myLatLng,
+    //    map: map,
+    //    title: 'Trash Here'
+    //});
+}
+
+window.mapDemo.initMap = initMap;
+window.mapDemo.addMarker = addMarker;
--- greenup.js
+++ greenup.js
@@ -1,19 +1,29 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+MarkerList = new Mongo.Collection('markers');
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+if (Meteor.isClient) {
+        function getLocation() {
+                navigator.geolocation.getCurrentPosition(showPosition);
+        }
+        function showPosition(position) {
+
+         MarkerList.insert({lat: position.coords.latitude, lng: position.coords.longitude, bags: 3, time: new Date()});
+            Session.set('currentLocation', {lat: position.coords.latitude, lng: position.coords.longitude});
+        }
+
+
+    Template.hello.helpers({
+        counter: function () {
+            return Session.get('counter');
+        }
+    });
 
   Template.hello.events({
-    'click button': function () {
+    'click .location-button': function () {
       // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+      getLocation();
+        window.mapDemo.addMarker(Session.get("currentLocation"));
     }
-  });
+  })
 }
 
 if (Meteor.isServer) {
