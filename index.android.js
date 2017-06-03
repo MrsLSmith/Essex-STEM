abd3faa25cb845f317fd3c4aa5dafaf6074c1af1
John Need <john.need@gmail.com>
2017-06-03 17:02:22 -0400

    reset

--- index.android.js
+++ index.android.js
@@ -1,60 +1,31 @@
 /**
- * GreenUpVermont React Native App
- * https://github.com/johnneed/GreenUpVermont
+ * Sample React Native App
+ * https://github.com/facebook/react-native
  * @flow
  */
 
 import React, { Component } from 'react';
-// import * as firebase from 'firebase';
-// import 'firebase/auth';
-// import 'firebase/database';
-// import 'firebase/storage';
-// import 'firebase-messaging';
- // import FireAuth from 'react-native-firebase-auth';
- import {
-  Alert,
+import {
   AppRegistry,
-  Button,
-  Image,
   StyleSheet,
   Text,
   View
 } from 'react-native';
-import logo from "./assets/GreenupVermontlogo.png"
-// Initialize Firebase
-// var config = {
-//   apiKey: "AIzaSyAjwSCpOvLPgYcFr26V3gmfwJlGb-VtWAs",
-//   authDomain: "greenupvermont-de02b.firebaseapp.com",
-//   databaseURL: "https://greenupvermont-de02b.firebaseio.com",
-//   projectId: "greenupvermont-de02b",
-//   storageBucket: "greenupvermont-de02b.appspot.com",
-//   messagingSenderId: "439621369113"
-// };
-const onButtonPress = () => {
-  Alert.alert('Button has been pressed!');
-};
-// var app = firebase.initializeApp(config);
-export default class GreenUpVermont extends Component {
+
+export default class GU extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Image
-          source={logo} />
         <Text style={styles.welcome}>
-          Welcome to the Green Up Vermont App!
+          Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
-         Congratulations you got this app running!
+          To get started, edit index.android.js
         </Text>
         <Text style={styles.instructions}>
           Double tap R on your keyboard to reload,{'\n'}
           Shake or press menu button for dev menu
         </Text>
-        <Button
-        onPress={onButtonPress}
-        title="Login with Google"
-        accessibilityLabel="Login With Google"
-      />
       </View>
     );
   }
@@ -79,4 +50,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('GreenUpVermont', () => GreenUpVermont);
+AppRegistry.registerComponent('GU', () => GU);
