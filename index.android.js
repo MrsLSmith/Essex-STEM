3f45bc0a3cfaa1a850684559fd652aa727ecf83d
John Need <john.need@gmail.com>
2017-06-03 19:50:46 -0400

    back to normal

--- index.android.js
+++ index.android.js
@@ -1,53 +1,71 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * GreenUpVermont React Native App
+ * https://github.com/johnneed/GreenUpVermont
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    Alert,
+    AppRegistry,
+    Button,
+    Image,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
-export default class GreenUpVermont extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+import logo from './assets/GreenupVermontlogo.png';
+
+const onButtonPress = () => {
+    Alert.alert('Button has been pressed!');
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5
+    },
 });
+
+export default class GreenUpVermont extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={logo} />
+                <Text style={styles.welcome}>
+                    Welcome to the Green Up Vermont App!
+                </Text>
+                <Text style={styles.instructions}>
+                    Congratulations you got this app running!
+                </Text>
+                <Text style={styles.instructions}>
+                    Double tap R on your keyboard to reload,{'\n'}
+                    Shake or press menu button for dev menu
+                </Text>
+                <Button
+                    onPress={onButtonPress}
+                    title="Login with Google"
+                    accessibilityLabel="Login With Google"
+                />
+            </View>
+        );
+    }
+}
+
+
 
 AppRegistry.registerComponent('GreenUpVermont', () => GreenUpVermont);
