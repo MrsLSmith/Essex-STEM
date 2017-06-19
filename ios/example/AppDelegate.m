1c9637f90f64f8ef82f9cdec6cf5d98114884316
John Need <john.need@gmail.com>
2017-06-19 16:29:50 -0400

    code push

--- ios/example/AppDelegate.m
+++ ios/example/AppDelegate.m
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import <CodePush/CodePush.h>
 #import <React/RCTBundleURLProvider.h>
 
 // **********************************************
@@ -17,7 +18,12 @@
 {
   NSURL *jsCodeLocation;
 #ifdef DEBUG
-//  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+//  
+#ifdef DEBUG
+    jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+#else
+    jsCodeLocation = [CodePush bundleURL];
+#endif
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 #else
    jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
