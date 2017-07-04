bca0f8e46286dd080698f6f837c7071e9812d6a5
Audrey <audrey.dawson99@gmail.com>
2017-07-03 20:20:46 -0400

    Navigator Buttons returned to page.

--- src/screens/trash-tracker/TrashTracker.js
+++ src/screens/trash-tracker/TrashTracker.js
@@ -51,6 +51,9 @@ export default class TrashTracker extends Component {
         //  this._myAwesomeMethod = this._myAwesomeMethod.bind(this);
         this.props.navigator.setOnNavigatorEvent(onNavigatorEvent(this.props.navigator).bind(this));
     }
+    componentDidMount() {
+        this.props.navigator.setButtons(navButtons);
+    }
     render() {
         return (
             <View style={styles.container}>
