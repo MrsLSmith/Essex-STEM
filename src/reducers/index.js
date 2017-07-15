8116275958346b0f8be242740b1fe4f856af1558
John Need <john.need@gmail>
2017-07-15 18:28:41 -0400

    need to merge

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import {combineReducers} from 'redux';
 import {messageReducer} from '../screens/messages/messageReducer';
 import {loginReducer} from '../screens/login/login-reducer';
+import {teamReducer} from '../screens/my-teams/team-reducers';
+
 const rootReducer = combineReducers({messageReducer, loginReducer});
 export default rootReducer;
--- src/screens/my-teams/my-teams.js
+++ src/screens/my-teams/my-teams.js
@@ -1,21 +1,14 @@
-/**
- * GreenUpVermont React Native App
- * https://github.com/johnneed/GreenUpVermont
- * @flow
- */
+// @flow
+
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {FontAwesome} from '@expo/vector-icons';
+import {Button, StyleSheet, Text, TouchableHighlight, View} from 'react-native';
+import {MaterialCommunityIcons} from '@expo/vector-icons';
+import * as teamActions from './team-actions';
 
-import {
-    Alert,
-    Button,
-    Image,
-    StyleSheet,
-    Text,
-    TouchableHighlight,
-    View
-} from 'react-native';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -23,33 +16,87 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
         width: '100%'
+    },
+    headerButton: {
+        // flex: 1,
+        width: 32
+    },
+    teams: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    },
+    inputStyle: {
+        paddingRight: 5,
+        paddingLeft: 5,
+        paddingBottom: 2,
+        color: '#262626',
+        fontSize: 18,
+        fontWeight: '200',
+        height: 40,
+        width: '100%',
+        textAlign: 'left',
+        borderColor: '#DDDDDD',
+        borderWidth: 1,
+        borderStyle: 'solid'
     }
 });
-export default class MyTeams extends Component {
-    static navigationOptions = {
-            title: 'My Teams'
+
+class TeamSummaries extends Component {
+    static propTypes = {
+        actions: PropTypes.object,
+        teams: PropTypes.array,
+        navigation: PropTypes.object,
+        toTeamDetails: PropTypes.function
     };
 
-    static propTypes = {
+    static navigationOptions = {
+        title: 'Teams'
     };
+
     constructor(props) {
         super(props);
-        this._myAwesomeMethod = this._myAwesomeMethod.bind(this);
-     }
-    componentDidMount() {
-     }
-    _myAwesomeMethod() {
-        Alert.alert('Huzzah!');
+        this.toTeamDetail = this
+            .toTeamDetail
+            .bind(this);
+    }
+
+    toTeamDetail(teamId: string) {
+        return () => {
+            this
+                .props
+                .navigation
+                .navigate('TeamDetails');
+        };
     }
     render() {
+
+        var myTeams = (this.props.teams || []).map(team => (
+            <TouchableHighlight
+                key={team._id}
+                onPress={this.toTeamDetail(team._id)}>
+                <View>
+                    <Text style={styles.teams}>{team.name}</Text>
+                </View>
+            </TouchableHighlight>
+        ));
         return (
             <View style={styles.container}>
-                <TouchableHighlight onPress={this._myAwesomeMethod} underlayColor={'rgba(0, 0, 0, 0.054)'}>
-                    <Text style={styles.text}>
-                        My Teams
-                    </Text>
-                </TouchableHighlight>
+                <Text>Team Summaries Screen</Text>
+                {myTeams}
             </View>
         );
     }
 }
+
+function mapStateToProps(state, ownProps) {
+    return {teams: state.teamReducer.teams};
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(teamActions, dispatch)
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TeamSummaries);
