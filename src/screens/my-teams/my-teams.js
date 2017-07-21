9485ecf04a79f78485f22f2c7956734a1d4f9860
Audrey <audrey.dawson99@gmail.com>
2017-07-21 16:28:26 -0400

    merge

--- src/screens/my-teams/my-teams.js
+++ src/screens/my-teams/my-teams.js
@@ -2,10 +2,12 @@
 
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {Button, FlatList, StyleSheet, Text, TouchableHighlight, View} from 'react-native';
 import {MaterialCommunityIcons} from '@expo/vector-icons';
 import * as teamActions from './team-actions';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
+
 
 const styles = StyleSheet.create({
     container: {
@@ -20,9 +22,10 @@ const styles = StyleSheet.create({
         width: 32
     },
     teams: {
+        justifyContent: 'flex-start',
+        alignItems: 'center',
         fontSize: 20,
-        textAlign: 'center',
-        margin: 10
+        margin: 5
     },
     inputStyle: {
         paddingRight: 5,
@@ -49,17 +52,22 @@ class TeamSummaries extends Component {
     };
 
     static navigationOptions = {
-        title: 'My Teams'
+        title: 'Teams'
     };
 
     constructor(props) {
         super(props);
         this.toTeamDetail = this.toTeamDetail.bind(this);
         this.toTeamSearch = this.toTeamSearch.bind(this);
+        this.toMessageTeam = this.toMessageTeam.bind(this);
     }
 
     toTeamSearch() {
         this.props.navigation.navigate('TeamSearch');
+    }
+
+    toMessageTeam() {
+        this.props.navigation.navigate('MessageTeam');
     }
 
     toTeamDetail(team: Object) {
@@ -80,12 +88,31 @@ class TeamSummaries extends Component {
         };
     }
 
+    toTeamIcon(team: Object) {
+        switch (true) {
+            case team.invitationPending:
+                return 'contact-mail';
+            case team.userIsOwner:
+                return 'pencil-box';
+            default:
+                return 'arrow-right-thick';
+        }
+    }
+
     render() {
 
         var myTeams = (this.props.teams || []).map(team => (
             <TouchableHighlight key={team._id} onPress={this.toTeamDetail(team)}>
                 <View>
+                    <TouchableHighlight onPress={this.toMessageTeam}>
+                        <MaterialCommunityIcons name = 'message-text-outline'
+                            size = '25'
+                        />
+                    </TouchableHighlight>
                     <Text style={styles.teams}>{team.name}</Text>
+                    <MaterialCommunityIcons name = {this.toTeamIcon(team)}
+                        size = '25'
+                    />
                 </View>
             </TouchableHighlight>
         ));
