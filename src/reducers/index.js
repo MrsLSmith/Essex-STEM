cf7cb0b445a102a53af135fb63b109d01464431f
Audrey <audrey.dawson99@gmail.com>
2017-07-24 07:22:19 -0400

    welcome import

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import {combineReducers} from 'redux';
 import {messageReducer} from '../screens/messages/messageReducer';
+import Welcome from './screens/login/';
 const rootReducer = combineReducers({messageReducer});
 export default rootReducer;
