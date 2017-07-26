fc77f40741af595257e4e2d47187e296c884a990
John Need <john.need@gmail>
2017-07-26 02:22:07 -0400

    bug fix

--- src/screens/my-teams/team-editor-details.js
+++ src/screens/my-teams/team-editor-details.js
@@ -5,11 +5,19 @@
  */
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {Button, StyleSheet, Text, TouchableHighlight, View, FlatList} from 'react-native';
+import {
+    Button,
+    StyleSheet,
+    Text,
+    TouchableHighlight,
+    View,
+    FlatList
+} from 'react-native';
 import {MaterialCommunityIcons} from '@expo/vector-icons';
 import * as teamActions from './team-actions';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
+import {SegmentedControls} from 'react-native-radio-buttons'
 
 const styles = StyleSheet.create({
     container: {
@@ -40,32 +48,32 @@ export default class TeamEditorDetails extends Component {
 
     constructor(props) {
         super(props);
-    }
-
-    setSelectedOption() {
-    }
-
-    render() {
-        let options = [
+        this.options = [
             {
                 label: 'Public',
                 value: 'public'
-            },
-            {
+            }, {
                 label: 'Private',
                 value: 'private'
             }
-        ]
+        ];
+        this.setSelectedOption = this.setSelectedOption.bind(this);
+        this.state = {
+            selectedOption: this.options[0]
+        };
+    }
+
+    setSelectedOption(option) {
+        console.log(option);
+        this.setState({selectedOption: option});
+    }
+
+    render() {
+
         return (
             <View style={styles.container}>
-                <SegmentedControls
-                    options={options}
-                    onSelection={setSelectedOption.bind(this)}
-                    selectedOption={this.state.selectedOption}
-                    extractText={(option) => option.label}
-                />
+                <SegmentedControls options={this.options} onSelection={this.setSelectedOption} selectedOption={this.state.selectedOption} selectedTint={'#EFEFEF'} tint={'#666666'} extractText={(option) => option.label}/>
             </View>
-        )
-            ;
+        );
     }
 }
