5446415933d3944c480627021014382e40e25cbb
Smaranda Fournier <smaranda.onutu@gmail.com>
2018-02-25 18:51:36 -0500

    Fixed merge issues

--- screens/messages-screen/index.js
+++ screens/messages-screen/index.js
@@ -83,55 +83,77 @@ class Messages extends Component {
     }
 
     render() {
-        const messages = this.props.messages;
-        const messageKeys = Object.keys(messages || {});
-        const sortedKeys = messageKeys.sort((key1, key2) => messages[key2].created.valueOf() - messages[key1].created.valueOf());
-        const myMessages = sortedKeys.map(key =>
-            (
-                <View key={key}>
-                    <TouchableHighlight onPress={this.toMessageDetail(key)}>
-                        <View style={[styles.message,
-                            messages[key].read ?
-                                styles.read : styles.unread]}>
-                            <Text style={messages[key].read ?
-                                styles.oldMsg : styles.newMsg}>
-                                {messages[key].text.length > 80 ?
-                                    `${messages[key].text.slice(0, 80)}...` :
-                                    messages[key].text}
-                            </Text>
-                            <Text style={{fontSize: 10, textAlign: 'right'}}>{messages[key].sender.email}</Text>
-                        </View>
-                    </TouchableHighlight>
-                </View>
-            )
-        );
+        if(this.props.teamsLoaded && this.props.messagesLoaded) {
+            const messages = this.props.messages;
+            const messageKeys = Object.keys(messages || {});
+            const sortedKeys = messageKeys.sort((key1, key2) => messages[key2].created.valueOf() - messages[key1].created.valueOf());
+            const myMessages = sortedKeys.map(key =>
+                (
+                    <View key={key}>
+                        <TouchableHighlight onPress={this.toMessageDetail(key)}>
+                            <View style={[styles.message,
+                                messages[key].read ?
+                                    styles.read : styles.unread]}>
+                                <Text style={messages[key].read ?
+                                    styles.oldMsg : styles.newMsg}>
+                                    {messages[key].text.length > 80 ?
+                                        `${messages[key].text.slice(0, 80)}...` :
+                                        messages[key].text}
+                                </Text>
+                                <Text style={{fontSize: 10, textAlign: 'right'}}>{messages[key].sender.email}</Text>
+                            </View>
+                        </TouchableHighlight>
+                    </View>
+                )
+            );
 
+            return this.props.userHasTeams ? (
+                <ScrollView style={styles.container}>
+                    <Button
+                        onPress={() => {
+                            this.props.navigation.navigate('NewMessage');
+                        }}
+                        title='New Message'
+                    />
+                    <View>
+                        {myMessages.length > 0 ? myMessages : (<Text>Sorry, no messages </Text>)}
+                    </View>
+                </ScrollView>
+            ) : (
+                <View style={styles.container}>
+                    <Text style={defaultStyles.heading2}>Whoops, you're lonely in here. 
+                        It seems like you don't have any teams to send messages to. Start your own team or join an existing one.
+                    </Text>
+                    <Button
+                        onPress={() => { this.props.navigation.navigate('Teams'); }}
+                        title='Go to my teams'
+                    />
+                </View>
+            );
+        }
 
         return (
-            <ScrollView style={styles.container}>
-                <Button
-                    onPress={() => {
-                        this.props.navigation.navigate('NewMessage');
-                    }}
-                    title='New Message'
-                />
-                {myMessages.length > 0 ? myMessages : (<Text>Sorry, no messages </Text>)}
-            </ScrollView>
-
+            <View>
+                <Text>Running around, gathering data... please wait</Text>
+            </View>
         );
     }
 }
 
-const mapStateToProps = (state) => {
-    const currentUser = state.login.user;
-    return {messages: state.messages.messages, currentUser};
-
-};
-
-const mapDispatchToProps = (dispatch) => {
+function mapStateToProps(state) {
     return {
-        actions: bindActionCreators(actions, dispatch)
+        messages: state.messages.messages,
+        currentUser: state.login.user,
+        userHasTeams: Object.values(state.teams.teams)
+            .filter(team => typeof team.members.find(member => member.uid === state.login.user.uid) !== 'undefined')
+            .length > 0,
+        teamsLoaded: state.messages.teamsLoaded,
+        messagesLoaded: state.messages.loaded
     };
-};
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    actions: bindActionCreators(actions, dispatch)
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Messages);
