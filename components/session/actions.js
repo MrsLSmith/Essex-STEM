d3a3b52e48b70ddf35721cfedf28c1abdbd50c5a
John Need <johnneed@johnneed.com>
2019-03-24 10:04:39 -0400

    hot fix - syntax error in new-team

--- components/session/actions.js
+++ components/session/actions.js
@@ -1,29 +1,5 @@
 import * as firebaseDataLayer from '../../data-sources/firebase-data-layer';
-import * as types from '../../constants/actionTypes';
-
-
-export function getCurrentUser() {
-    return (dispatch) => {
-        firebaseDataLayer.getCurrentUser(dispatch);
-    };
-}
-
-export function loadingFailed(error) {
-    return {type: types.LOADING_FAILED, isLoadingComplete: true, error: error};
-}
-
-export function loadingCompleted() {
-    return {type: types.LOADING_COMPLETED, isLoadingComplete: true};
-}
 
 export function initialize() {
     return (dispatch: Object => any) => firebaseDataLayer.initialize(dispatch);
-}
-
-export function isLoggingInViaSSO(_isLoggingInViaSSO: Boolean) {
-    return {type: types.IS_LOGGING_IN_VIA_SSO, isLoggingInViaSSO: _isLoggingInViaSSO};
-}
-
-export function updateEmail(email) {
-    return {type: types.EMAIL_UPDATE, email};
 }
