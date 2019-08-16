864ba69528643ff64357e6b6a63f8e2197ac249f
John Need <john.need@gmail.com>
2019-08-16 18:07:29 -0400

    redo firebase-config

---
+++ data-sources/firebase-config.js
@@ -0,0 +1,6 @@
+// @flow
+import getEnvVars from "../environment";
+
+const { apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId } = getEnvVars();
+
+export const firebaseConfig = { apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId };
