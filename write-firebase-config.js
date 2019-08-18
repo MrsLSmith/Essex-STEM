a21bc1858e8c01b27961054160f93bc464e09f9e
John Need <john.need@gmail.com>
2019-08-18 18:39:10 -0400

    CI adding publishing

--- write-firebase-config.js
+++ write-firebase-config.js
@@ -3,15 +3,15 @@ const fs = require("fs");
 // eslint-disable-next-line no-process-env
 const apiKey = process.env.FIREBASE_API_KEY;
 // eslint-disable-next-line no-process-env
-const authDomain = process.env.FIREBASE_API_KEY;
+const authDomain = process.env.FIREBASE_AUTH_DOMAIN;
 // eslint-disable-next-line no-process-env
-const databaseURL = process.env.FIREBASE_API_KEY;
+const databaseURL = process.env.FIREBASE_DATABASE_URL;
 // eslint-disable-next-line no-process-env
-const projectId = process.env.FIREBASE_API_KEY;
+const projectId = process.env.FIREBASE_PROJECT_ID;
 // eslint-disable-next-line no-process-env
-const storageBucket = process.env.FIREBASE_API_KEY;
+const storageBucket = process.env.FIREBASE_STORAGE_BUCKET;
 // eslint-disable-next-line no-process-env
-const messagingSenderId = process.env.FIREBASE_API_KEY;
+const messagingSenderId = process.env.FIREBASE_MESSAGING_SENDER_ID;
 
 const firebaseConfig = {
     apiKey,
