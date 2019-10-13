7898baf7066b6cac3e6216487f3f81a11261c5e5
John Need <johnneed@johnneed.com>
2019-10-13 17:46:13 -0400

    bug fix

--- components/home-button/home-button.js
+++ components/home-button/home-button.js
@@ -1,31 +1,32 @@
-import React from 'react';
+import React from "react";
 import {
+    View,
     StyleSheet,
     ImageBackground,
     Text,
     TouchableHighlight
-} from 'react-native';
-import * as colors from '../../styles/constants';
+} from "react-native";
+import * as colors from "../../styles/constants";
 
 const styles = StyleSheet.create({
     homeButton: {
-        width: '50%',
+        width: "50%",
         aspectRatio: 1,
         borderWidth: 3,
-        borderStyle: 'solid',
+        borderStyle: "solid",
         borderColor: colors.backgroundDark
     },
-    buttonImage: {width: '100%', height: '100%'},
+    buttonImage: { width: "100%", height: "100%" },
     homeButtonText: {
-        position: 'absolute',
+        position: "absolute",
         bottom: 0,
         left: 0,
-        backgroundColor: '#FA774EAA',
-        width: '100%',
+        backgroundColor: "#FA774EAA",
+        width: "100%",
         padding: 2,
-        color: '#FFF',
+        color: "#FFF",
         fontSize: 20,
-        textAlign: 'center'
+        textAlign: "center"
     }
 });
 
@@ -36,18 +37,18 @@ type PropsType = {
     id?: string
 };
 
-export const HomeButton = ({label, backgroundImage, onPress, id}: PropsType) => (
+export const HomeButton = ({ label, backgroundImage, onPress, id }: PropsType) => (
     <TouchableHighlight
-        id={id}
-        style={styles.homeButton}
-        onPress={onPress}
+        id={ id }
+        style={ styles.homeButton }
+        onPress={ onPress }
     >
-        <View style={{height: '100%', width: '100%', borderStyle: 'solid', borderWidth: 1, borderColor: '#555'}}>
+        <View style={ { height: "100%", width: "100%", borderStyle: "solid", borderWidth: 1, borderColor: "#000" } }>
             <ImageBackground
-                style={styles.buttonImage}
-                source={backgroundImage}
+                style={ styles.buttonImage }
+                source={ backgroundImage }
             >
-                <Text style={styles.homeButtonText}>{label}</Text>
+                <Text style={ styles.homeButtonText }>{label}</Text>
             </ImageBackground>
         </View>
     </TouchableHighlight>
