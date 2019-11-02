aabb24ef5ef0ef4aa98858ee8850fabb1e9155eb
John Need <johnneed@johnneed.com>
2019-11-02 07:44:25 -0400

    layout

--- screens/free-supplies-screen/index.js
+++ screens/free-supplies-screen/index.js
@@ -1,13 +1,21 @@
 // @flow
 import React, { useState } from "react";
-import { StyleSheet, KeyboardAvoidingView, ScrollView, View, FlatList, TextInput, Platform, Text } from "react-native";
+import { StyleSheet, ScrollView, View, FlatList, TextInput, TouchableHighlight, Platform } from "react-native";
 import { connect } from "react-redux";
 import { defaultStyles } from "../../styles/default-styles";
 import PickupLocation from "../../components/pickup-location";
 import * as R from "ramda";
 import WatchGeoLocation from "../../components/watch-geo-location";
+import { Ionicons } from "@expo/vector-icons";
+import colors from "../../constants/colors";
 
 const styles = StyleSheet.create(defaultStyles);
+const iconStyle = {
+    height: 40,
+    width: 40,
+    padding: 2,
+    color: "white"
+};
 
 type PropsType = {
     towns: { [key: string]: Town },
@@ -29,21 +37,38 @@ const FreeSupplies = ({ towns, userLocation }: PropsType): React$Element<any> =>
 
         <View style={ styles.frame }>
             <WatchGeoLocation/>
-            <View style={ { margin: 10 } }>
-                <TextInput
-                    keyBoardType={ "default" }
-                    onChangeText={ onSearchTermChange(towns) }
-                    placeholder={ "Search by City/Town" }
-                    style={ styles.textInput }
-                    value={ searchTerm }
-                    underlineColorAndroid={ "transparent" }
-                />
+            <View style={ { margin: 10, marginBottom: 2, height: 40, backgroundColor: "red" } }>
+                <View style={ { flex: 1, flexDirection: "row", justifyContent: "flexStart" } }>
+                    <View style={ { flex: 1, flexDirection: "column" } }>
+                        <TextInput
+                            keyBoardType={ "default" }
+                            onChangeText={ onSearchTermChange(towns) }
+                            placeholder={ "Search" }
+                            style={ [styles.textInput, { alignSelf: "stretch" }] }
+                            value={ searchTerm }
+                            underlineColorAndroid={ "transparent" }
+                        />
+                    </View>
+                    <TouchableHighlight style={ { height: 40, width: 40 } }>
+                        <Ionicons
+                            name={ Platform.OS === "ios" ? "ios-close" : "md-close" }
+                            size={ 36 }
+                            style={ iconStyle }/>
+                    </TouchableHighlight>
+                    <TouchableHighlight style={ { height: 40, width: 40 } }>
+                        <Ionicons
+                            name={ Platform.OS === "ios" ? "ios-locate" : "md-locate" }
+                            size={ 36 }
+                            style={ iconStyle }/>
+                    </TouchableHighlight>
+                </View>
             </View>
             <ScrollView style={ styles.scroll }>
                 <View style={ styles.infoBlockContainer }>
                     <FlatList
                         style={ styles.infoBlockContainer }
                         data={ searchTerm ? searchResults : towns }
+                        keyExtractor={ item => item.key }
                         renderItem={ ({ item }: { item: Town }): React$Element<any> => (
                             <PickupLocation item={ item }/>) }/>
                 </View>
@@ -59,8 +84,9 @@ FreeSupplies.navigationOptions = {
 
 const mapStateToProps = (state: Object): Object => {
 
-    const flatReduce = ([key, town]) => (town.pickupLocations || []).map(pickup => ({
+    const flatReduce = ([key, town]) => (town.pickupLocations || []).map((pickup, i): Object => ({
         ...pickup,
+        key: i,
         townId: key,
         townName: town.name
     }));
