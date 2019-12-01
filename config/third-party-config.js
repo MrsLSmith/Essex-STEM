068c30226eb7264075f0c1fcabd35caf795ae3be
John Need <johnneed@johnneed.com>
2019-12-01 09:45:23 -0500

    fix build

--- config/third-party-config.js
+++
@@ -1,5 +0,0 @@
-import getEnvVars from "../environment";
-
-const { androidClientId, androidStandaloneAppClientId, iosClientId, iosStandaloneAppClientId, facebookAppId } = getEnvVars();
-
-export const thirdPartyConfig = { androidClientId, androidStandaloneAppClientId, iosClientId, iosStandaloneAppClientId, facebookAppId };
