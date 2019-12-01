288316e9178177c8ba8177a09dcbee66e4b0ce25
John Need <johnneed@johnneed.com>
2019-12-01 10:37:31 -0500

    build fixes

--- action-creators/session-action-creators.js
+++ action-creators/session-action-creators.js
@@ -1,10 +1,10 @@
 // @flow
 
 import * as types from "../constants/action-types";
-import { Google, Facebook } from "expo";
+// import { Google, Facebook } from "expo";
 import * as firebaseDataLayer from "../data-sources/firebase-data-layer";
-import { thirdPartyConfig } from "../config/third-party-config";
-import { Platform } from "react-native";
+// import { thirdPartyConfig } from "../config/third-party-config";
+// import { Platform } from "react-native";
 
 export const logout = (): ThunkType => {
     function thunk(dispatch: Dispatch<ActionType>) {
@@ -41,63 +41,63 @@ export function loginWithEmailPassword(email: string, password: string): ThunkTy
     return thunk;
 }
 
-// TODO Refactor this into a true Thunk
-export function googleLogin(): (Dispatch<Object> => Promise<any>) {
-    return async function logIn(dispatch: Dispatch<ActionType>) {
-        try {
-            // Expo SDK 32 has a bug. Swap clientId logic if you are in dev or pushing a standalone app to TestFlight or App store
-            const result = await Google.logInAsync({
-                clientId: Platform.OS === "android" ? thirdPartyConfig.androidClientId : thirdPartyConfig.iosClientId, // use this line when pushing to app store.
-                // clientId: thirdPartyConfig.androidClientId,  // use this line when in development (using Expo App)
-                scopes: ["profile", "email"],
-                useBrowser: true,
-                behavior: "web"
-            });
-
-            if (result.type === "success") {
-                firebaseDataLayer.googleAuth(result.idToken, dispatch).catch((error: Error) => {
-                    dispatch({
-                        type: types.LOGIN_FAIL,
-                        error
-                    });
-                });
-            } else {
-                dispatch({
-                    type: types.LOGIN_FAIL,
-                    error: "Google authentication failed"
-                });
-            }
-        } catch (error) {
-            dispatch({ type: types.LOGIN_FAIL, error });
-        }
-    };
-}
+// // TODO Refactor this into a true Thunk
+// export function googleLogin(): (Dispatch<Object> => Promise<any>) {
+//     return async function logIn(dispatch: Dispatch<ActionType>) {
+//         try {
+//             // Expo SDK 32 has a bug. Swap clientId logic if you are in dev or pushing a standalone app to TestFlight or App store
+//             const result = await Google.logInAsync({
+//                 clientId: Platform.OS === "android" ? thirdPartyConfig.androidClientId : thirdPartyConfig.iosClientId, // use this line when pushing to app store.
+//                 // clientId: thirdPartyConfig.androidClientId,  // use this line when in development (using Expo App)
+//                 scopes: ["profile", "email"],
+//                 useBrowser: true,
+//                 behavior: "web"
+//             });
+//
+//             if (result.type === "success") {
+//                 firebaseDataLayer.googleAuth(result.idToken, dispatch).catch((error: Error) => {
+//                     dispatch({
+//                         type: types.LOGIN_FAIL,
+//                         error
+//                     });
+//                 });
+//             } else {
+//                 dispatch({
+//                     type: types.LOGIN_FAIL,
+//                     error: "Google authentication failed"
+//                 });
+//             }
+//         } catch (error) {
+//             dispatch({ type: types.LOGIN_FAIL, error });
+//         }
+//     };
+// }
 
 // TODO: Refactor into a true Thunk
-export function facebookLogin(): (Dispatch<Object> => Promise<any>) {
-    return async function logIn(dispatch: Object => *) {
-        const facebook = await Facebook.logInWithReadPermissionsAsync(thirdPartyConfig.facebookAppId, {
-            behavior: "web", permissions: ["public_profile", "email"]
-        });
-        const { type, token } = facebook;
-        if (type === "success") {
-            firebaseDataLayer.facebookAuth(token, dispatch).catch((error: Error) => {
-                dispatch({
-                    type: types.LOGIN_FAIL,
-                    error
-                });
-            });
-        } else {
-            dispatch({
-                type: types.LOGIN_FAIL,
-                session: {
-                    facebook: null,
-                    user: null
-                }
-            });
-        }
-    };
-}
+// export function facebookLogin(): (Dispatch<Object> => Promise<any>) {
+//     return async function logIn(dispatch: Object => *) {
+//         const facebook = await Facebook.logInWithReadPermissionsAsync(thirdPartyConfig.facebookAppId, {
+//             behavior: "web", permissions: ["public_profile", "email"]
+//         });
+//         const { type, token } = facebook;
+//         if (type === "success") {
+//             firebaseDataLayer.facebookAuth(token, dispatch).catch((error: Error) => {
+//                 dispatch({
+//                     type: types.LOGIN_FAIL,
+//                     error
+//                 });
+//             });
+//         } else {
+//             dispatch({
+//                 type: types.LOGIN_FAIL,
+//                 session: {
+//                     facebook: null,
+//                     user: null
+//                 }
+//             });
+//         }
+//     };
+// }
 
 export function resetPassword(emailAddress: string): ThunkType {
     function thunk(dispatch: Dispatch<ActionType>) {
--- data-sources/firebase-data-layer.js
+++ data-sources/firebase-data-layer.js
@@ -8,7 +8,7 @@ import Town from "../models/town";
 import Message from "../models/message";
 import Invitation from "../models/invitation";
 import * as types from "../constants/action-types";
-import { firebaseConfig } from "./firebase-config";
+import { firebaseConfig } from "../firebase-config";
 import "firebase/firestore";
 import { curry } from "ramda";
 import * as messageTypes from "../constants/message-types";
