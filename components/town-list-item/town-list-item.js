62b21faafaba17b249e08421b2e620ccb707366c
Micah Mutrux <micah.mutrux@gmail.com>
2020-03-29 00:03:38 -0400

    town-list-item.js

--- components/town-list-item/town-list-item.js
+++ components/town-list-item/town-list-item.js
@@ -2,39 +2,35 @@
 import React from "react";
 import { TouchableOpacity } from "react-native";
 import { SimpleLineIcons } from "@expo/vector-icons";
-import { Text, View } from "@shoutem/ui";
+import { Text } from "@shoutem/ui";
 
 type PropsType = { town: { townName: string }, onClick: any => void };
 
 export const TownListItem = ({ town, onClick }: PropsType): React$Element<any> => (
-        <TouchableOpacity
-            onPress={ onClick }
-            style={ {
-                flex: 1,
-                flexDirection: "row",
-                borderBottomWidth: 1,
-                borderColor: "#AAA",
-                padding: 20,
-                justifyContent: "flex-start"
-            } }>
-
-            <Text style={ {
-                textAlign: "center",
-                fontWeight: "bold",
-                color: "#111",
-                fontSize: 20,
-                fontFamily: "Rubik-Regular",
-                flexGrow: 1
-            } }>
-                { town.townName || "" }
-            </Text>
-            <SimpleLineIcons
-                name={ "arrow-right" }
-                size={ 20 }
-                color="#333"
-            />
-
-        </TouchableOpacity>
-    )
-;
-
+    <TouchableOpacity
+        onPress={ onClick }
+        style={ {
+            flex: 1,
+            flexDirection: "row",
+            borderBottomWidth: 1,
+            borderColor: "#AAA",
+            padding: 20,
+            justifyContent: "flex-start"
+        } }>
+        <Text style={ {
+            textAlign: "center",
+            fontWeight: "bold",
+            color: "#111",
+            fontSize: 20,
+            fontFamily: "Rubik-Regular",
+            flexGrow: 1
+        } }>
+            { town.townName || "" }
+        </Text>
+        <SimpleLineIcons
+            name={ "arrow-right" }
+            size={ 20 }
+            color="#333"
+        />
+    </TouchableOpacity>
+);
