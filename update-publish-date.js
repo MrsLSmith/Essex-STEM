83b4fc308a95ffbe2148fdaa247ed7bcdd9ed15a
foo <john.need@gmail.com>
2020-03-31 09:13:12 -0400

    new ci

---
+++ update-publish-date.js
@@ -0,0 +1,7 @@
+const jsonfile = require("jsonfile");
+const file = "package.json";
+const data = jsonfile.readFileSync(file);
+const date = new Date();
+const publishDate = `${date.getYear() + 1900}-${date.getMonth() + 1}-${date.getDate()}`;
+data.publishDate = publishDate;
+jsonfile.writeFileSync(file, data, { spaces: 2, EOL: "\r\n" });
